38191698d6d037fbf4e91e0ee9be24fb46ab5162
Mauricio Avila <mao979797@gmail.com>
2021-03-17 15:20:07 +0000

    Change company card logo

--- resources/views/livewire/company-logo.blade.php
+++ resources/views/livewire/company-logo.blade.php
@@ -1,1 +1,1 @@
-<img class="w-auto h-auto max-w-48 max-h-48" src="https://images.pexels.com/photos/5145/animal-easter-chick-chicken.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500">
+<img class="w-auto h-auto max-w-48 max-h-48" src="https://images.pexels.com/photos/2777898/pexels-photo-2777898.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
