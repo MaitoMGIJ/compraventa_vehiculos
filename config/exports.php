24e706d67c6a6afff69c43c1de1a7ec1f449feb3
Mauricio Avila <mao979797@gmail.com>
2021-03-21 06:50:48 +0000

    Export XLS

--- config/exports.php
+++ config/exports.php
@@ -15,6 +15,18 @@ return [
             'date' => 'transactions.date',
             'agent' => 'agents.name',
             'commission' => 'transactions.commission'
-        ]
+        ],
+        'xls' => [
+            'id' => 'transactions.id',
+            'transaction_type' => 'transaction_types.description as transaction_type',
+            'license' => 'vehicles.license',
+            'brand' => 'vehicle_brands.description as vehicle_brand',
+            'reference' => 'vehicle_references.description as vehicle_reference',
+            'model' => 'vehicles.model',
+            'value' => 'transactions.value',
+            'date' => 'transactions.date',
+            'agent' => 'agents.name',
+            'commission' => 'transactions.commission'
+        ],
     ]
 ];
