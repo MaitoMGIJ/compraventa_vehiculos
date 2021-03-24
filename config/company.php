ac219085db163104626e223d10aa2869b8cbe8b3
Mauricio Avila <mao979797@gmail.com>
2021-03-24 14:10:58 +0000

    Company logo

---
+++ config/company.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'name' => 'Data Ingeniería de Colombia',
+    'an_application_of' => 'Una Aplicación de'
+];
--- resources/views/livewire/company-card-logo.blade.php
+++ resources/views/livewire/company-card-logo.blade.php
@@ -4,8 +4,8 @@
           <livewire:company-logo class="h-48 w-full object-cover md:w-48" />
       </div>--}}
       <div class="flex flex-col flex-grow p-8 items-center justify-items-center">
-        <div class="uppercase text-sm flex-grow text-indigo-500 font-semibold">Una aplicación de</div>
-        <div class="flex-grow justify-items-center"><a href="#" class="block mt-1 w-full text-lg leading-tight font-medium text-black hover:underline">Chicken Company</a></div>
+        <div class="uppercase text-sm flex-grow text-indigo-500 font-semibold">{{ config('company.an_application_of') }}</div>
+        <div class="flex-grow justify-items-center"><span class="block mt-1 w-full text-lg leading-tight font-medium text-black hover:underline">{{ config('company.name') }}</span></div>
       </div>
     </div>
   </div>
