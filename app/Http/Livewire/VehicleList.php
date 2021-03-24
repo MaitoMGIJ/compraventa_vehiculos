c89513caed546c09b42f19766a819a04a1abd5e4
Mauricio Avila <mao979797@gmail.com>
2021-03-24 05:50:23 +0000

    Filter active/inactive in vehicle list

--- app/Http/Livewire/VehicleList.php
+++ app/Http/Livewire/VehicleList.php
@@ -9,13 +9,14 @@ use Illuminate\Support\Str;
 class VehicleList extends Component
 {
     public $searchTerm = '';
+    public $active = true;
 
     public function render()
     {
         $vehicles = [];
         $this->searchTerm = Str::upper($this->searchTerm);
         $vehicles = Vehicle::where('license', 'like', "%{$this->searchTerm}%")
-            ->where('is_active', true)
+            ->where('is_active', $this->active)
             ->orderBy('created_at', 'desc')
             ->paginate(9);
         return view('livewire.vehicle-list', [
--- resources/views/livewire/vehicle-list.blade.php
+++ resources/views/livewire/vehicle-list.blade.php
@@ -1,6 +1,14 @@
 <div wire:loading.class="opacity-25">
-    <div class="flex flex-col flwx-grow justify-items-center items-center text-center pb-5">
-        <input type="text" placeholder="{{ __('tags.search').' '.__('tags.license') }}" class="self-center form-input md:w-1/3 w-full rounded-md shadow-sm text-center" wire:model="searchTerm"/>
+    <div class="md:inline-flex space-y-4 md:space-y-0 w-full p-4 text-gray-500 items-center">
+        <div class="md:w-1/2 max-w-sm mx-auto">
+            <select wire:model="active" name="active" id="active" class="bg-gray-200 text-gray-900 appearance-none border-1 inline-block py-3 pl-3 pr-8 rounded leading-tight w-full">
+                <option value="1" selected>{{ __('tags.active') }}</option>
+                <option value="0">{{ __('tags.inactive') }}</option>
+            </select>
+        </div>
+        <div class="md:w-1/2 max-w-sm mx-auto">
+            <input type="text" placeholder="{{ __('tags.search').' '.__('tags.license') }}" class="self-center form-input w-full rounded-md shadow-sm text-center" wire:model="searchTerm"/>
+        </div>
     </div>
     <div class="grid md:grid-cols-3 sm:grid-cols-1 gap-4 mt-8">
         @foreach($vehicles as $vehicle)
